8e3ac85ea388aa791de0163fb4df4d84e7f43b4b
Anekantjainsagar <anekantjainsagar@gmail.com>
2024-07-30 19:46:18 +0530

    Resume updated

--- src/app/Components/Sections/LandingPage.jsx
+++ src/app/Components/Sections/LandingPage.jsx
@@ -125,7 +125,7 @@ const LandingPage = ({ location }) => {
             cnt={0}
             onClick={(e) => {
               window.open(
-                "https://drive.google.com/file/d/1vJW6ZVb9eN2ScTW8e_ZS9awnmJ-H5rUS/view?usp=sharing"
+                "https://drive.google.com/file/d/1PM6hbptgtiSEyWJnjcNyj_iLNKcIC-Jh/view?usp=sharing"
               );
             }}
           />
